df13eafbb2ee80355aedcde4b192b887eafcaf97
Giovanni Lima <giovannilimadev@gmail.com>
2022-06-22 23:10:41 -0300

    feat: config eslint, prettier, styled components, globalstyles reset

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './Pages/Home'
+import { GlobalStyle } from './Styles/globalStyle'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  )
+}
+
+export default App
