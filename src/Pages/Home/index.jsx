661fb5e143d0ad59eeca88d423d0b844d1f1eaf5
Giovanni Lima <giovannilimadev@gmail.com>
2022-06-24 13:56:39 -0300

    feat: insert text and images in new layout

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,58 @@
+import people from '../../assets/people.png'
 import * as S from './styles'
 
 export default function Home() {
   return (
-    <>
-      <h1>AYLA & CO</h1>
-      <S.Container />
-    </>
+    <S.Container>
+      <S.Logo>AYLA&CO</S.Logo>
+      <S.NavBar>
+        <S.NavItems>
+          <a href="google.com">Serviços e Consultoria</a>
+        </S.NavItems>
+        <S.NavItems>
+          <a href="google.com">Serviços</a>
+        </S.NavItems>
+        <S.NavItems>
+          <a href="google.com">Conheça</a>
+        </S.NavItems>
+        <S.NavItems>
+          <a href="google.com">Contate a AYLA&CO</a>
+        </S.NavItems>
+      </S.NavBar>
+      <S.Content>
+        <span>
+          <S.Title>
+            Serviços de terceirização de processos de negócios (BPO) para
+            continuidade de negócios e resiliência
+          </S.Title>
+          <S.Text>
+            Em tempos desafiadores, veja como nossos especialistas em
+            terceirização de processos de negócios podem criar a continuidade de
+            negócios para oferecer melhores fluxos de trabalho inteligentes para
+            a transformação de sua empresa.
+          </S.Text>
+        </span>
+        <span>
+          <img src={people} alt="people" />
+        </span>
+      </S.Content>
+      <S.Divider />
+
+      <S.ContentMid>
+        <S.Title>O que é a BPO para fluxos de trabalho inteligente?</S.Title>
+        <S.TextMid>
+          As tecnologias digitais criaram novas oportunidades para os líderes de
+          negócios. Os modelos de terceirização de processos de negócios e
+          operações tradicionais estão sendo reinventados por meio de fluxos de
+          trabalho mais inteligentes, utilizando automação, inteligência
+          artificial, Internet das Coisas (IoT), cloud e outras tecnologias
+          emergentes. Para continuar relevante e crescer,as empresas devem
+          evoluir constantemente para obter processos mais inteligentes e
+          orquestrados, que levem em consideração os clientes, os funcionários,
+          os fornecedores e os parceiros de negócios.
+        </S.TextMid>
+      </S.ContentMid>
+      <S.Divider />
+    </S.Container>
   )
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,7 +1,79 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: red;
+  margin: 0 auto;
+  width: 100%;
+`
+export const Logo = styled.div`
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+`
+
+export const NavBar = styled.div`
+  display: flex;
+  width: 100%;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  justify-content: space-between;
+  padding: 0.5rem 4rem;
+`
+export const NavItems = styled.div`
+  a {
+    text-decoration: none;
+    color: #383838;
+  }
+`
+
+export const Content = styled.div`
+  height: 250px;
+  max-width: 1024px;
+  margin: 0 auto;
+  display: flex;
+  padding: 2rem 8rem;
+  justify-content: space-between;
+
+  span:first-child {
+    max-width: 425px;
+  }
+
+  img {
+    height: 240px;
+  }
+`
+export const ContentMid = styled.div`
+  height: 250px;
+  max-width: 1024px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 8rem 0;
+
+  strong {
+    max-width: 300px;
+    flex-shrink: 0;
+  }
+`
+
+export const Title = styled.strong`
+  font-size: 1.5rem;
+  letter-spacing: 0.2px;
+`
+
+export const Text = styled.p`
+  margin-top: 2rem;
+  font-size: 1rem;
+  letter-spacing: 0.2px;
+  height: 72px;
+`
+export const TextMid = styled.p`
+  font-size: 1rem;
+  letter-spacing: 0.2px;
+`
+
+export const Divider = styled.div`
+  margin-top: 3rem;
+  background-color: #383838;
+  height: 2.5rem;
+  width: 100%;
 `
